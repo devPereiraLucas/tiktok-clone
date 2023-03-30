392ea40e9bdbe6dd8ac29f3207a857eaa4a61046
Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-03-29 21:17:08 -0300

    adjusting video in screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import Video from './pages/Video';
 
 function App() {
   return (
     <div className="App">
+      <div className="app__videos">
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
