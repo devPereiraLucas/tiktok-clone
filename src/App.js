cb0b87771206525ee7e4977fb0c243c3c28f282c
Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-03-31 23:08:09 -0300

    passing video properties to the base App

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={ 146 }
+          shares={ 4 }
+          messages={ 198 }
+          name="lucas_pereira"
+          description="#TattooDesenhoEmNeon"
+          song="put ur head on my shoulder - cat"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video 
+          likes={ 532 }
+          shares={ 0 }
+          messages={ 134 }
+          name="lucas_pereira"
+          description="#TattooDesenhoEmNeon"
+          song="put ur head on my shoulder - cat"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
       </div>
     </div>
   );
