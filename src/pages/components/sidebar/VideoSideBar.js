bec394c71c196ac8a6c94b8a220ccf877a1f2dcf
Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-03-31 23:07:06 -0300

    creating sidebar for video page

--- src/pages/components/sidebar/VideoSideBar.js
+++ src/pages/components/sidebar/VideoSideBar.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./VideoSideBar.css";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MessageIcon from '@mui/icons-material/Message';
+import ShareIcon from '@mui/icons-material/Share';
 
-function VideoSideBar() {
+function VideoSideBar({ likes, shares, messages }) {
+  const [liked, setLiked] = useState(false)
+
+  const handleLike = () => {
+    setLiked(!liked)
+  }
+  
   return (
-    <div>VideoSideBar</div>
+    <div className="video__sidebar">
+      <div 
+        className="video__sidebar__options"
+        onClick={handleLike}
+      >
+        { liked 
+          ? <FavoriteIcon className="" fontSize="large"/> 
+          : <FavoriteBorderIcon className="" fontSize="large"/>
+        }
+        <p>{liked ? likes + 1 : likes}</p>
+      </div>
+      <div className="video__sidebar__options">
+        <MessageIcon className="" fontSize="large"/>
+        <p>{messages}</p>
+      </div>
+      <div className="video__sidebar__options">
+        <ShareIcon className="" fontSize="large"/>
+        <p>{shares}</p>
+      </div>
+    </div>
   )
 }
 
