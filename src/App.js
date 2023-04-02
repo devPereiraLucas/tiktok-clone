44fdc12faef42f418e778d2ba9dd384f4412da7b
Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-04-01 21:18:23 -0300

    creating getVideos function of firebase

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Video from './pages/Video';
+import db from './config/firebase';
+import { collection, getDocs } from 'firebase/firestore/lite';
 
 function App() {
+
+  const [video, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosCollection = collection(db, 'videos');
+    const videosSnapShot = await getDocs(videosCollection);
+    const videosList = videosSnapShot.docs.map(doc => doc.data());
+    setVideos(videosList);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
     <div className="App">
       <div className="app__videos">
-        <Video 
-          likes={ 146 }
-          shares={ 4 }
-          messages={ 198 }
-          name="lucas_pereira"
-          description="#TattooDesenhoEmNeon"
-          song="put ur head on my shoulder - cat"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
-        />
-        <Video 
-          likes={ 532 }
-          shares={ 0 }
-          messages={ 134 }
-          name="lucas_pereira"
-          description="#TattooDesenhoEmNeon"
-          song="put ur head on my shoulder - cat"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
-        />
+        { video.map((item) => {
+            return (
+              <Video 
+                likes={ item.likes }
+                shares={ item.shares }
+                messages={ item.messages }
+                name={ item.name }
+                description={ item.description }
+                song= { item.song }
+                url=  { item.url }
+              />
+            )
+          })
+        }
       </div>
     </div>
   );
